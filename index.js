d270c26fb7c575274a0cc62b212f49ca4ccabfbe
Mr.mobin <barmar.mobin@gmail.com>
2022-09-22 23:37:09 +0330

    getting contact information added

--- index.js
+++ index.js
@@ -1,4 +1,41 @@
-const command = process.argv[2]
+// import yargs from 'yargs';
 
-if(command == 'add') console.log('Adding ...');
-else console.log('command not valid!');
+const yargs = require('yargs');
+
+yargs.command({
+    command: 'create',
+    aliases: ['c'],
+    describe: '[create new contact]',
+    builder: {
+        fullname: {
+            alias: 'f',
+            describe: 'Person fullname',
+            demandOption: true,
+            type: 'string'
+        },
+
+        phone: {
+            alias: 'p',
+            describe: 'Person Phone Number',
+            demandOption: true,
+            type: 'number'
+        },
+
+        email: {
+            alias: 'e',
+            describe: 'Person Email Address',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler({fullname, phone, email}){
+        console.log(fullname, phone, email);
+    }
+})
+
+
+// console.log(yargs.argv);
+
+yargs.parse()
+
+console.log(yargs.argv);
